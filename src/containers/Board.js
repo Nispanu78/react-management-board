eb5fb6d83987e1bccd7164774034e8ec59ef1670
Nicola <nicspanu@gmail.com>
2021-12-03 12:09:07 +0100

    react

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import withDataFetching from '../withDataFetching';
 import Lane from '../components/Lane/Lane';
 
 const BoardWrapper = styled.div`
@@ -13,56 +14,17 @@ const BoardWrapper = styled.div`
   }
 `;
 
-class Board extends Component {
-  constructor() {
-    super()
-    this.state = {
-      data: [],
-      loading: true,
-      error: '',
-    }
-  }
-
-  async componentDidMount() {
-    try{
-      const tickets = await fetch('../../assets/data.json');
-      const ticketsJson = await tickets.json();
-
-      if (ticketsJson) {
-        this.setState({
-          data:ticketsJson,
-          loading: false,
-        });
-      }
-  } catch(error) {
-    this.setState({
-      loading: false,
-      error: error.message,
-      });
-    }
-  }
-  render() {
-    const { data, loading, error } = this.state;
-    
-    const lanes = [
-      { id: 1, title: 'To Do' },
-      { id: 2, title: 'In Progress' },
-      { id: 3, title: 'Review' },
-      { id: 4, title: 'Done' },
-    ];
-
-    return (
-      <BoardWrapper>
-        {lanes.map(lane => (
-          <Lane key={lane.id} 
-          title={lane.title}
-          loading={loading}
-          error={error}
-          tickets={data.filter(ticket => ticket.lane ===lane.id)} />
-        ))}
-      </BoardWrapper>
-    );
-  }
-}
-
-export default Board;
+const Board = ({ lanes, loading, error, data }) => (
+  <BoardWrapper>
+    {lanes.map(lane =>
+      <Lane
+        key={lane.id}
+        title={lane.title}
+        loading={loading}
+        error={error}
+        tickets={data.filter(ticket => ticket.lane === lane.id)}
+      />
+    )}
+  </BoardWrapper>
+);
+export default withDataFetching(Board);
